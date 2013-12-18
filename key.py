9cdb5f65d3852090302acbcdc714c4d34fa0416b
KurtHornett <thehornettb@kurts-air.home>
2013-12-18 22:39:56 +0000

    Twitter CLI started and updated
    Started CLI for twitter interaction before imlpemeting it into the GUI
    in order to get a graspe of using new module.

---
+++ key.py
@@ -0,0 +1,5 @@
+import pickle
+
+with open('.consumer_key.txt','wb') as key:
+    pickle.dump('YuE239eH7r1wKsGj8KIPTdIXRCipdNaBVHu7al3sF7I',key)
+
---
+++ twitter-cli.py
@@ -0,0 +1,95 @@
+#Kurt Hornett
+#COMP4 - Implemetation
+#Twitter Implemetation work - MarkII
+#Dec 2013
+
+from twitter import *
+from sqlite3 import *
+
+import os
+import pickle
+
+#Creation of a CLI Inteface for interacting with twitter.
+#Limiting to viewing 10 latest tweets from timeline for testing purposes.
+
+def mainMenu():
+    #Typical main menu 
+    print()
+    print('Twitter Management Program')
+    print()
+    print('1. Authorize application')
+    print('2. Get Home Timeline')
+    print('3. Print 10 latest Tweets')
+    print('0. Exit Application')
+    print()
+
+def getChoice():
+    choice = int(input('Select number: '))
+    return choice
+
+def getConsumerKey():
+    #Get Consumer key from hidden file - API advises it's kept secret
+    with open('.consumer_key.txt','rb') as key:
+        consumer_key = pickle.load(key)
+    return consumer_key
+
+def twitterLogIn(consumer_key):
+    #All necessary proceedures for authorising app and therefore loggin into twitter, doesn't allow log out.
+    #Uses PIN authorization  - could change to UserPass later in app stages and implemetation
+    MY_TWITTER_CREDS = os.path.expanduser('~/.login_credentials')
+    if not os.path.exists(MY_TWITTER_CREDS):
+        oauth_dance("KurtsApp",'Y4zkic6lw0Hu6uB3sNVH4Q', consumer_key,
+                        MY_TWITTER_CREDS)
+    oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
+
+    #Creation of twitter object with authorized detailes.
+    twitter = Twitter(auth=OAuth(
+        oauth_token, oauth_secret, 'Y4zkic6lw0Hu6uB3sNVH4Q', consumer_key))
+    return twitter
+
+def getHomeTimeline(twitter):
+    #Creates home_timeline object with all available tweets from the home timeline. 
+    home_timeline = twitter.statuses.home_timeline()
+    return home_timeline
+
+def displayLatestTweets(hm):
+    #Print information of 10 latest tweets as a table
+    print('{0:<3}{1:<15}{2:<140}{3:<21}'.format('No.','Screen Name','Tweet Text','Links'))
+    count = 0
+    while count < 10:
+        if hm[count]['entities']['urls'][0]['url'] != '':
+            print('{0:<3}{1:<15}{2:<140}{3:<21}'.format(count+1,hm[count]['user']['screen_name'],
+                                                            hm[count]['text'],
+                                                            hm[count]['entities']['urls'][0]['url']))
+        else:
+            print('{0:<3}{1:<15}{2:<140}{3:<21}'.format(count+1,hm[count]['user']['screen_name'],
+                                                            hm[count]['text'],
+                                                            'null'))
+            
+        count += 1
+              
+def displayLinks(hm):
+    print('{0:>3}{1:>21}'.format('No.','Link'))
+    count = 0
+    while count < 10:
+        print('{0:>3}{1:>21}'.format(count+1,hm[count]['entities']['urls'][0]['url']))
+        count += 1
+
+if __name__ == "__main__":
+    #Consumer key required regardless
+    consumer_key = getConsumerKey()
+    choice = 1
+    #Typical while loop for managing program.
+    #0 being catch-out for exiting program.
+    while choice != 0:
+        mainMenu()
+        choice = getChoice()
+        if choice == 1:
+            twitter = twitterLogIn(consumer_key)
+        if choice == 2:
+            home_timeline = getHomeTimeline(twitter)
+        if choice == 3:
+            displayLinks(home_timeline)
+        
+        
+    
