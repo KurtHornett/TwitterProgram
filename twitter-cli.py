1877d77a25d4298d079e4b68059edad7647b8762
KurtHornett <kurthornett@yahoo.co.uk>
2013-12-19 10:04:54 +0000

    Updated Twitter CLI

--- twitter-cli.py
+++ twitter-cli.py
@@ -3,9 +3,14 @@
 #Twitter Implemetation work - MarkII
 #Dec 2013
 
+#External Modules
 from twitter import *
 from sqlite3 import *
 
+#User Made Maodules
+
+
+#System Modules
 import os
 import pickle
 
@@ -52,33 +57,39 @@ def getHomeTimeline(twitter):
     home_timeline = twitter.statuses.home_timeline()
     return home_timeline
 
-def displayLatestTweets(hm):
+def displayLatestTweets(hm,count):
     #Print information of 10 latest tweets as a table
     print('{0:<3}{1:<15}{2:<140}{3:<21}'.format('No.','Screen Name','Tweet Text','Links'))
-    count = 0
     while count < 10:
-        if hm[count]['entities']['urls'][0]['url'] != '':
+        if hm[count]['entities']['urls'] != []:
             print('{0:<3}{1:<15}{2:<140}{3:<21}'.format(count+1,hm[count]['user']['screen_name'],
                                                             hm[count]['text'],
                                                             hm[count]['entities']['urls'][0]['url']))
         else:
             print('{0:<3}{1:<15}{2:<140}{3:<21}'.format(count+1,hm[count]['user']['screen_name'],
                                                             hm[count]['text'],
-                                                            'null'))
+                                                            'No Link'))
             
         count += 1
               
-def displayLinks(hm):
-    print('{0:>3}{1:>21}'.format('No.','Link'))
-    count = 0
+def displayLinks(hm,count):
+    print('{0:<3} {1:<21}'.format('No.','Link'))
     while count < 10:
-        print('{0:>3}{1:>21}'.format(count+1,hm[count]['entities']['urls'][0]['url']))
+        if hm[count]['entities']['urls'] != []:
+            print('{0:<3} {1:<21}'.format(count+1,hm[count]['entities']['urls'][0]['url']))
+        else:
+             print('{0:<3} {1:<21}'.format(count+1,'No Link'))
+        count += 1
+def checkingStuff(hm,count):
+    while count <= 10:
+        print(hm[count]['entities']['urls'])
         count += 1
 
 if __name__ == "__main__":
     #Consumer key required regardless
     consumer_key = getConsumerKey()
     choice = 1
+    count = 0
     #Typical while loop for managing program.
     #0 being catch-out for exiting program.
     while choice != 0:
@@ -89,7 +100,10 @@ if __name__ == "__main__":
         if choice == 2:
             home_timeline = getHomeTimeline(twitter)
         if choice == 3:
-            displayLinks(home_timeline)
+            displayLatestTweets(home_timeline,count)
+        if choice == 9:
+            checkingStuff(home_timeline,count)
         
         
     
+
