f8653e15bcbe9cbb85b09123d3b77d6b98a6a9ff
KurtHornett <kurthornett@yahoo.co.uk>
2014-02-07 13:43:48 +0000

    Added Suggested User interface start

---
+++ SuggestedInterface.py
@@ -0,0 +1,20 @@
+#Kurt Hornett
+#COMP4 Implementation
+#Suggested Users Interface
+#February 2014
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtSql import *
+
+from sql_gui_misc import *
+from twitter_database import *
+
+import sys
+
+class SuggestedInteface(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        #set Window Title
+        self.setWindowTile('Suggested Users')
+        
